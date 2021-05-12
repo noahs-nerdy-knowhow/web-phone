cd44161b07f7bbc1ae78d7fb04c29574b5e26912
noahs-nerdy-knowhow <78937679+noahs-nerdy-knowhow@users.noreply.github.com>
2021-05-12 00:15:44 -0500

    Add code to change background

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 let colorScheme = 0;
 let background = document.getElementById("bg");
 
+background.onclick = switchColorScheme;
+
 function switchColorScheme() {
 	switch(colorScheme) {
 		case 0:
 			colorScheme = 1;
+			background.src = background.src.replace("-light", "-dark");
 			break;
 		case 1:
 			colorScheme = 0;
+			background.src = background.src.replace("-dark", "-light");
 			break;
 	}
 }
